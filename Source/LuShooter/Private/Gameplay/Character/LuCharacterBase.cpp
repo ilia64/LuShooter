e5f0a588c6e1465a623366ad89ac9363ce8a8194
Ilia Lukin <ilia64mods@gmail.com>
2025-11-20 16:17:32 +0300

    Character: init stgartup effect and abilities

--- Source/LuShooter/Private/Gameplay/Character/LuCharacterBase.cpp
+++ Source/LuShooter/Private/Gameplay/Character/LuCharacterBase.cpp
@@ -4,13 +4,15 @@
 #include "Gameplay/AbilitySystem/LuAbilitySystemComponent.h"
 #include "Gameplay/AbilitySystem/Attributes/HealthAttributeSet.h"
 
+DEFINE_LOG_CATEGORY(LogCharacter);
+
 ALuCharacterBase::ALuCharacterBase(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	AbilitySystemComponent = ObjectInitializer.CreateDefaultSubobject<ULuAbilitySystemComponent>(this, TEXT("AbilitySystemComponent"));
 	AbilitySystemComponent->SetIsReplicated(true);
 
-	HealthAttributeSet = ObjectInitializer.CreateDefaultSubobject<UHealthAttributeSet>(this, TEXT("HealthAttributes"));
+	HealthAttributeSet = ObjectInitializer.CreateDefaultSubobject<UHealthAttributeSet>(this, TEXT("HealthAttributeSet"));
 }
 
 UAbilitySystemComponent* ALuCharacterBase::GetAbilitySystemComponent() const
@@ -23,4 +25,88 @@ void ALuCharacterBase::PostInitializeComponents()
 	Super::PostInitializeComponents();
 
 	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+}
+
+void ALuCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		InitializeDefaultAbilities();
+		InitializeDefaultEffects();
+	}
+}
+
+void ALuCharacterBase::InitializeDefaultAbilities()
+{
+	check(AbilitySystemComponent);
+	check(HasAuthority());
+
+	for (TSubclassOf Ability : DefaultAbilities)
+	{
+		if (Ability.Get())
+		{
+			FGameplayAbilitySpecHandle Handle = AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec{Ability});
+			if (!Handle.IsValid())
+			{
+				UE_LOG(LogCharacter, Warning, TEXT("Failed to give ability %s to %s"), *GetNameSafe(Ability.Get()), *GetNameSafe(this));
+			}
+		}
+		else
+		{
+			UE_LOG(LogCharacter, Warning, TEXT("%s has null Ability in DefaultAbilities"), *GetNameSafe(this));
+		}
+	}
+}
+
+void ALuCharacterBase::InitializeDefaultEffects()
+{
+	check(HasAuthority());
+
+	FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+	EffectContext.AddSourceObject(this);
+
+	if (DefaultAttributes.Get())
+	{
+		const bool bResult = ApplyGameplayEffectToSelf(DefaultAttributes, EffectContext);
+		if (bResult == false)
+		{
+			UE_LOG(LogCharacter, Error, TEXT("Failed to apply DefaultAttributes: %s to %s"), *GetNameSafe(DefaultAttributes.Get()), *GetNameSafe(this));
+		}
+	}
+
+	for (TSubclassOf EffectClass : DefaultEffects)
+	{
+		if (EffectClass.Get())
+		{
+			const bool bResult = ApplyGameplayEffectToSelf(EffectClass, EffectContext);
+			if (bResult == false)
+			{
+				UE_LOG(LogCharacter, Error, TEXT("Failed to apply %s to %s from DefaultEffects"), *GetNameSafe(EffectClass), *GetNameSafe(this));
+			}
+		}
+		else
+		{
+			UE_LOG(LogCharacter, Warning, TEXT("%s has null gameplayEffect in DefaultEffects"), *GetNameSafe(this));
+		}
+	}
+}
+
+bool ALuCharacterBase::ApplyGameplayEffectToSelf(const TSubclassOf<UGameplayEffect>& EffectClass, const FGameplayEffectContextHandle& EffectContext) const
+{
+	check(HasAuthority());
+	check(AbilitySystemComponent);
+	check(EffectClass.Get());
+	check(EffectContext.IsValid());
+
+	const FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(EffectClass, 1, EffectContext);
+	if (SpecHandle.IsValid())
+	{
+		const FActiveGameplayEffectHandle EffectHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+		return EffectHandle.WasSuccessfullyApplied();
+	}
+
+	UE_LOG(LogCharacter, Error, TEXT("Failed to apply %s to %s. SpecHandle not valid!"), *GetNameSafe(EffectClass.Get()), *GetNameSafe(this));
+	return false;
 }
--- Source/LuShooter/Public/Gameplay/Character/LuCharacterBase.h
+++ Source/LuShooter/Public/Gameplay/Character/LuCharacterBase.h
@@ -7,8 +7,13 @@
 #include "GameFramework/Character.h"
 #include "LuCharacterBase.generated.h"
 
+struct FGameplayEffectContextHandle;
+class UGameplayEffect;
+class UGameplayAbility;
 class ULuAbilitySystemComponent;
 class UHealthAttributeSet;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogCharacter, Verbose, All);
 
 UCLASS(Abstract)
 class LUSHOOTER_API ALuCharacterBase : public ACharacter, public IAbilitySystemInterface
@@ -23,9 +28,26 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 	virtual void PostInitializeComponents() override;
+	virtual void BeginPlay() override;
+
+	bool ApplyGameplayEffectToSelf(const TSubclassOf<UGameplayEffect>& EffectClass, const FGameplayEffectContextHandle& EffectContext) const;
+
+private:
+	void InitializeDefaultAbilities();
+	void InitializeDefaultEffects();
+
+private:
+	UPROPERTY(EditAnywhere, Category="Abilities")
+	TArray<TSubclassOf<UGameplayAbility>> DefaultAbilities;
+
+	UPROPERTY(EditAnywhere, Category="Abilities")
+	TSubclassOf<UGameplayEffect> DefaultAttributes;
+
+	UPROPERTY(EditAnywhere, Category="Abilities")
+	TArray<TSubclassOf<UGameplayEffect>> DefaultEffects;
 
 protected:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Ability")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Abilities")
 	TObjectPtr<ULuAbilitySystemComponent> AbilitySystemComponent;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Attributes")
