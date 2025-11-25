7166aa07c8fdfba0e131b2afd8446d860c974df9
Ilia Lukin <ilia64mods@gmail.com>
2025-11-25 20:29:51 +0300

    Ability: add effect6s to Base LuGameplayAbility

---
+++ Source/LuShooter/Private/Gameplay/AbilitySystem/Ability/LuGameplayAbility.cpp
@@ -0,0 +1,65 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Gameplay/AbilitySystem/Ability/LuGameplayAbility.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemLog.h"
+#include "Gameplay/Character/LuCharacterBase.h"
+
+ALuCharacterBase* ULuGameplayAbility::GetAvatarCharacterFromActorInfo() const
+{
+	return Cast<ALuCharacterBase>(GetAvatarActorFromActorInfo());
+}
+
+void ULuGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (ActorInfo->IsNetAuthority())
+	{
+		const ALuCharacterBase* Character = GetAvatarCharacterFromActorInfo();
+
+		FGameplayEffectContextHandle ContextHandle = Character->GetGameplayEffectContextSelf();
+		ContextHandle.SetAbility(this);
+
+		for (const auto& Effect : ActivationEffects)
+		{
+			const FActiveGameplayEffectHandle EffectHandle = Character->ApplyGameplayEffectToSelf(Effect, ContextHandle);
+			if (!EffectHandle.WasSuccessfullyApplied())
+			{
+				UE_LOG(LogGameplayEffects, Error, TEXT("Failed to apply activation effect %s from ability %s to character %s"), *GetNameSafe(Effect.Get()), *GetNameSafe(this), *GetNameSafe(Character));
+			}
+		}
+
+		for (const auto& Effect : OngoingEffects)
+		{
+			const FActiveGameplayEffectHandle EffectHandle = Character->ApplyGameplayEffectToSelf(Effect, ContextHandle);
+			if (EffectHandle.WasSuccessfullyApplied())
+			{
+				OngoingEffectHandles.Add(EffectHandle);
+			}
+			else
+			{
+				UE_LOG(LogGameplayEffects, Error, TEXT("Failed to apply ongoing effect %s from ability %s to character %s"), *GetNameSafe(Effect.Get()), *GetNameSafe(this), *GetNameSafe(Character));
+			}
+		}
+	}
+}
+
+void ULuGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	if (ActorInfo->IsNetAuthority())
+	{
+		for (const auto& GameplayEffectHandle : OngoingEffectHandles)
+		{
+			if (GameplayEffectHandle.IsValid())
+			{
+				ActorInfo->AbilitySystemComponent->RemoveActiveGameplayEffect(GameplayEffectHandle);
+			}
+		}
+		OngoingEffectHandles.Empty();
+	}
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/LuShooter/Public/Gameplay/AbilitySystem/Ability/LuGameplayAbility.h
@@ -0,0 +1,32 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "LuGameplayAbility.generated.h"
+
+class ALuCharacterBase;
+
+UCLASS(Abstract)
+class LUSHOOTER_API ULuGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintPure)
+	ALuCharacterBase* GetAvatarCharacterFromActorInfo() const;
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="Effects")
+	TArray<TSubclassOf<UGameplayEffect>> OngoingEffects;
+
+	UPROPERTY(EditDefaultsOnly, Category="Effects")
+	TArray<TSubclassOf<UGameplayEffect>> ActivationEffects;
+
+private:
+	TArray<FActiveGameplayEffectHandle> OngoingEffectHandles;
+};
