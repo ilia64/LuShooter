06f839b4c13a65280bb06e217961bc294d81e103
Ilia Lukin <ilia64mods@gmail.com>
2025-12-05 15:53:21 +0300

    UI Common: AttributeProgressBar

---
+++ Source/LuShooter/Private/UI/Common/AttributeProgressBar.cpp
@@ -0,0 +1,46 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+
+#include "UI/Common/AttributeProgressBar.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "Components/Image.h"
+
+void UAttributeProgressBar::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	ProgressBar_Image->SetRenderTransformPivot({0.f, 0.f});
+	ProgressBar_Image->SetRenderScale({0.5, 1.f});
+}
+
+void UAttributeProgressBar::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	AbilitySystemComponent = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(GetOwningPlayerPawn());
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(ValueAttribute).AddUObject(this, &ThisClass::OnAttributeChanged);
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(MaxValueAttribute).AddUObject(this, &ThisClass::OnAttributeChanged);
+
+	UpdateView();
+}
+
+void UAttributeProgressBar::OnAttributeChanged(const FOnAttributeChangeData& Data)
+{
+	if (Data.NewValue == Data.OldValue)
+	{
+		return;
+	}
+
+	UpdateView();
+}
+
+void UAttributeProgressBar::UpdateView() const
+{
+	const float Value = AbilitySystemComponent->GetNumericAttribute(ValueAttribute);
+	const float MaxValue = AbilitySystemComponent->GetNumericAttribute(MaxValueAttribute);
+	const float Percent = Value / MaxValue;
+
+	ProgressBar_Image->SetRenderScale({Percent, 1.f});
+}
---
+++ Source/LuShooter/Public/UI/Common/AttributeProgressBar.h
@@ -0,0 +1,40 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "Blueprint/UserWidget.h"
+#include "AttributeProgressBar.generated.h"
+
+struct FOnAttributeChangeData;
+class UImage;
+
+UCLASS()
+class LUSHOOTER_API UAttributeProgressBar : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativePreConstruct() override;
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="123")
+	FGameplayAttribute ValueAttribute;
+
+	UPROPERTY(EditAnywhere, Category="123")
+	FGameplayAttribute MaxValueAttribute;
+
+	UPROPERTY(EditDefaultsOnly, meta=(BindWidget))
+	TObjectPtr<UImage> ProgressBar_Image;
+
+private:
+	void OnAttributeChanged(const FOnAttributeChangeData& Data);
+
+	void UpdateView() const;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
