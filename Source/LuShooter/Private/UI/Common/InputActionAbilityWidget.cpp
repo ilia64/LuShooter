3ff47b943f5adbb6c0b8ee82b4799054a3cc5213
Ilia Lukin <ilia64mods@gmail.com>
2025-12-08 18:39:28 +0300

    UI Common: Ability Widget with Cooldown

---
+++ Source/LuShooter/Private/UI/Common/InputActionAbilityWidget.cpp
@@ -0,0 +1,113 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+
+#include "UI/Common/InputActionAbilityWidget.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
+
+void UInputActionAbilityWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (!ensureAlways(InputTag.IsValid()))
+	{
+		return;
+	}
+
+	AbilitySystemComponent = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(GetOwningPlayerPawn());
+	if (ensureAlways(AbilitySystemComponent))
+	{
+		AbilitySystemComponent->AbilityActivatedCallbacks.AddUObject(this, &ThisClass::OnAbilityActivated);
+		AbilitySystemComponent->AbilityFailedCallbacks.AddUObject(this, &ThisClass::OnAbilityFailed);
+	}
+}
+
+void UInputActionAbilityWidget::OnAbilityActivated(UGameplayAbility* GameplayAbility)
+{
+	const FGameplayAbilitySpec* Spec = GameplayAbility->GetCurrentAbilitySpec();
+	if (!Spec || !Spec->DynamicAbilityTags.HasTag(InputTag))
+	{
+		return;
+	}
+
+	ensureAlways(!CurrentAbility);
+	ensureAlways(!CooldownUpdateTimeHandle.IsValid());
+
+	CurrentAbility = GameplayAbility;
+	CurrentAbility->OnGameplayAbilityEnded.AddUObject(this, &ThisClass::OnAbilityEnded);
+	CurrentAbility->OnGameplayAbilityCancelled.AddUObject(this, &ThisClass::OnAbilityCancelled);
+
+	K2_AbilityActivated();
+}
+
+void UInputActionAbilityWidget::OnAbilityEnded(UGameplayAbility* GameplayAbility)
+{
+	K2_AbilityEnded();
+
+	const bool bHasCooldown = TryStartCooldown();
+	if (!bHasCooldown)
+	{
+		ClearCurrentAbility();
+	}
+}
+
+void UInputActionAbilityWidget::OnAbilityCancelled()
+{
+	ClearCurrentAbility();
+}
+
+bool UInputActionAbilityWidget::TryStartCooldown()
+{
+	float TimeRemaining;
+	float CooldownDuration;
+	CurrentAbility->GetCooldownTimeRemainingAndDuration(CurrentAbility->GetCurrentAbilitySpecHandle(), CurrentAbility->GetCurrentActorInfo(), TimeRemaining, CooldownDuration);
+
+	if (TimeRemaining > CooldownMinimumTime)
+	{
+		K2_CooldownStarted();
+		K2_CooldownUpdated(TimeRemaining, CooldownDuration);
+
+		GetWorld()->GetTimerManager().SetTimer(CooldownUpdateTimeHandle, this, &ThisClass::OnCooldownTimerTick, CooldownUpdateTime, true);
+
+		return true;
+	}
+
+	return false;
+}
+
+void UInputActionAbilityWidget::OnCooldownTimerTick()
+{
+	float TimeRemaining;
+	float CooldownDuration;
+	CurrentAbility->GetCooldownTimeRemainingAndDuration(CurrentAbility->GetCurrentAbilitySpecHandle(), CurrentAbility->GetCurrentActorInfo(), TimeRemaining, CooldownDuration);
+
+	if (TimeRemaining > CooldownMinimumTime)
+	{
+		K2_CooldownUpdated(TimeRemaining, CooldownDuration);
+	}
+	else
+	{
+		GetWorld()->GetTimerManager().ClearTimer(CooldownUpdateTimeHandle);
+
+		K2_CooldownEnded();
+
+		ClearCurrentAbility();
+	}
+}
+
+void UInputActionAbilityWidget::OnAbilityFailed(const UGameplayAbility* GameplayAbility, const FGameplayTagContainer& GameplayTags)
+{
+	const FGameplayAbilitySpec* Spec = GameplayAbility->GetCurrentAbilitySpec();
+	if (Spec || Spec->DynamicAbilityTags.HasTag(InputTag))
+	{
+		K2_AbilityFailed();
+	}
+}
+
+void UInputActionAbilityWidget::ClearCurrentAbility()
+{
+	CurrentAbility->OnGameplayAbilityEnded.RemoveAll(this);
+	CurrentAbility->OnGameplayAbilityCancelled.RemoveAll(this);
+	CurrentAbility = nullptr;
+}
---
+++ Source/LuShooter/Public/UI/Common/InputActionAbilityWidget.h
@@ -0,0 +1,73 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Abilities/GameplayAbility.h"
+#include "Blueprint/UserWidget.h"
+#include "InputActionAbilityWidget.generated.h"
+
+
+class UImage;
+class UAbilitySystemComponent;
+
+UCLASS()
+class LUSHOOTER_API UInputActionAbilityWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName="AbilityActivated")
+	void K2_AbilityActivated();
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName="AbilityEnded")
+	void K2_AbilityEnded();
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName="AbilityCanceled")
+	void K2_AbilityCanceled();
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName="AbilityFailed")
+	void K2_AbilityFailed();
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName="CooldownStarted")
+	void K2_CooldownStarted();
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName="CooldownUpdated")
+	void K2_CooldownUpdated(const float Remaining, const float TotalDuration);
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName="CooldownEnded")
+	void K2_CooldownEnded();
+
+protected:
+	UPROPERTY(EditAnywhere, Category="123")
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditAnywhere, Category="123")
+	float CooldownMinimumTime{0.05f};
+
+	UPROPERTY(EditAnywhere, Category="123")
+	float CooldownUpdateTime{0.05f};
+
+private:
+	void ClearCurrentAbility();
+
+	void OnAbilityActivated(UGameplayAbility* GameplayAbility);
+	void OnAbilityFailed(const UGameplayAbility* GameplayAbility, const FGameplayTagContainer& GameplayTags);
+	void OnAbilityEnded(UGameplayAbility* GameplayAbility);
+	void OnAbilityCancelled();
+
+	bool TryStartCooldown();
+	void OnCooldownTimerTick();
+
+private:
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UGameplayAbility> CurrentAbility;
+
+	FTimerHandle CooldownUpdateTimeHandle;
+};
