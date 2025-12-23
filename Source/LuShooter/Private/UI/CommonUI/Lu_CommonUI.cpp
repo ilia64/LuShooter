f18947fee7ca781e290ec1529726996e19c1e07e
Ilia Lukin <ilia64mods@gmail.com>
2025-12-23 20:16:09 +0300

    CommonUI: Add Layer Tags

---
+++ Source/LuShooter/Private/UI/CommonUI/Lu_CommonUI.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+
+#include "UI/CommonUI/Lu_CommonUI.h"
+
+UE_DEFINE_GAMEPLAY_TAG(UITag::Layer_Game, "UI.Layer.Game");
+UE_DEFINE_GAMEPLAY_TAG(UITag::Layer_Menu, "UI.Layer.Menu");
+UE_DEFINE_GAMEPLAY_TAG(UITag::Layer_Modal, "UI.Layer.Modal");
+UE_DEFINE_GAMEPLAY_TAG(UITag::Layer_Tooltip, "UI.Layer.Tooltip");
---
+++ Source/LuShooter/Public/UI/CommonUI/Lu_CommonUI.h
@@ -0,0 +1,22 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+/**  
+ *  Приоритет:
+ *   Tooltip  — 1000  (самый верхний: tooltip, hint on hover)
+ *   Modal    — 600   (попапы, пауза, подтверждения)
+ *   Menu     — 300   (лобби, инвентарь, карта — non-gameplay UI)
+ *   Game     — 100   (HUD: прицел, здоровье, мана, хоткеи абилок)
+ */
+
+namespace UITag
+{
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Layer_Game);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Layer_Menu);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Layer_Modal);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Layer_Tooltip);
+}
