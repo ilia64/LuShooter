b02e1a87705c00696ebceb5ba4ce35ae4b40389d
Ilia Lukin <ilia64mods@gmail.com>
2025-12-24 00:00:01 +0300

    CommonUI: Create base Layout with Layers and map Tags

---
+++ Source/LuShooter/Private/UI/CommonUI/PrimaryGameLayout.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+
+#include "UI/CommonUI/PrimaryGameLayout.h"
+#include "GameplayTagContainer.h"
+
+
+void UPrimaryGameLayout::RegisterLayer(const FGameplayTag Tag, UCommonActivatableWidgetContainerBase* Container)
+{
+	if (!IsDesignTime())
+	{
+		if (!LayerByTag.Contains(Tag))
+		{
+			LayerByTag.Add(Tag, Container);
+		}
+	}
+}
---
+++ Source/LuShooter/Public/UI/CommonUI/PrimaryGameLayout.h
@@ -0,0 +1,27 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "GameplayTagContainer.h"
+#include "PrimaryGameLayout.generated.h"
+
+class UCommonActivatableWidgetContainerBase;
+
+UCLASS(Abstract, meta=(DisableNativeTick))
+class LUSHOOTER_API UPrimaryGameLayout : public UCommonUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	FORCEINLINE UCommonActivatableWidgetContainerBase* GetLayer(const FGameplayTag Tag) const { return LayerByTag.FindRef(Tag); }
+
+protected:
+	UFUNCTION(BlueprintCallable)
+	void RegisterLayer(UPARAM(meta = (Categories = "UI.Layer")) const FGameplayTag Tag, UCommonActivatableWidgetContainerBase* Container);
+
+private:
+	UPROPERTY(Transient, meta = (Categories = "UI.Layer"))
+	TMap<FGameplayTag, UCommonActivatableWidgetContainerBase*> LayerByTag;
+};
