49b46ef4042af423a003d9ce4f0e2a3d3e383856
Ilia Lukin <ilia64mods@gmail.com>
2026-01-14 18:55:21 +0300

    CommonUI: BP node - PushWidget AsyncAction

---
+++ Source/LuShooter/Private/UI/Actions/AsyncAction_PushWidget.cpp
@@ -0,0 +1,51 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+
+#include "UI/Actions/AsyncAction_PushWidget.h"
+
+#include "GameplayTagContainer.h"
+#include "System/CommonUISubsystem.h"
+
+
+UAsyncAction_PushWidget* UAsyncAction_PushWidget::PushWidget(const UObject* WorldContextObject, APlayerController* PlayerController, const TSoftClassPtr<UBaseActivatableWidget> WidgetClass, const FGameplayTag LayerTag, const bool bFocusOnNewlyPushedWidget)
+{
+	if (!ensureAlways(GEngine))
+	{
+		return nullptr;
+	}
+
+	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::Assert);
+	if (!ensureAlways(World))
+	{
+		return nullptr;
+	}
+
+	UAsyncAction_PushWidget* Node = NewObject<UAsyncAction_PushWidget>();
+	Node->OwningWorld = World;
+	Node->OwningPlayerController = PlayerController;
+	Node->OwningWidgetClass = WidgetClass;
+	Node->OwningLayerTag = LayerTag;
+	Node->bOwningFocusOnNewlyPushedWidget = bFocusOnNewlyPushedWidget;
+
+	Node->RegisterWithGameInstance(World);
+	return Node;
+}
+
+void UAsyncAction_PushWidget::Activate()
+{
+	UCommonUISubsystem* CommonUISubsystem = UCommonUISubsystem::Get(OwningWorld.Get());
+	CommonUISubsystem->PushWidgetAsync(OwningLayerTag, OwningWidgetClass, OwningPlayerController.Get(), [this](const EPushActivatableWidgetStatus Status, UBaseActivatableWidget* Widget)
+	{
+		switch (Status)
+		{
+		case Init:
+			OnInit.Broadcast(Widget);
+			
+		case Added:
+			OnPushed.Broadcast(Widget);
+
+		case Failed:
+			OnFailed.Broadcast();
+		}
+	});
+}
---
+++ Source/LuShooter/Public/UI/Actions/AsyncAction_PushWidget.h
@@ -0,0 +1,48 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "UI/CommonUI/BaseActivatableWidget.h"
+#include "AsyncAction_PushWidget.generated.h"
+
+class UBaseActivatableWidget;
+
+UCLASS()
+class LUSHOOTER_API UAsyncAction_PushWidget : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPushWidgetDelegate, UBaseActivatableWidget*, Widget);
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FPushWidgetFailedDelegate);
+
+public:
+	UFUNCTION(BlueprintCallable, DisplayName="Push Widget", meta = (WorldContext = "WorldContextObject", HidePin = "WorldContextObject", BlueprintInternalUseOnly = "true"), Category="123|Async")
+	static UAsyncAction_PushWidget* PushWidget(const UObject* WorldContextObject,
+	                                           APlayerController* PlayerController,
+	                                           const TSoftClassPtr<UBaseActivatableWidget> WidgetClass,
+	                                           UPARAM(meta = (Categories = "UI.Layer")) const FGameplayTag LayerTag,
+	                                           const bool bFocusOnNewlyPushedWidget = true);
+
+
+	virtual void Activate() override;
+
+	UPROPERTY(BlueprintAssignable)
+	FPushWidgetDelegate OnInit;
+
+	UPROPERTY(BlueprintAssignable)
+	FPushWidgetDelegate OnPushed;
+
+	UPROPERTY(BlueprintAssignable)
+	FPushWidgetFailedDelegate OnFailed;
+
+private:
+	TWeakObjectPtr<UWorld> OwningWorld;
+	TWeakObjectPtr<APlayerController> OwningPlayerController;
+	TSoftClassPtr<UBaseActivatableWidget> OwningWidgetClass;
+	FGameplayTag OwningLayerTag;
+	bool bOwningFocusOnNewlyPushedWidget{true};
+};
