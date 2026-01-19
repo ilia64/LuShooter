c66d4c645b0b3916869fdb6a85cad6f046089a43
Ilia Lukin <ilia64mods@gmail.com>
2026-01-19 16:31:34 +0300

    CommonUI: BP FunctionLibrary - GetWidgetClassByTag

---
+++ Source/LuShooter/Private/UI/CommonUI/CommonUI_FunctionLibrary.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+
+#include "UI/CommonUI/CommonUI_FunctionLibrary.h"
+
+#include "System/CommonUISubsystem.h"
+#include "UI/CommonUI/CommonUI_DeveloperSettings.h"
+
+TSoftClassPtr<UBaseActivatableWidget> UCommonUI_FunctionLibrary::GetWidgetClassByTag(const FGameplayTag Tag)
+{
+	if (!ensureAlways(Tag.IsValid()))
+	{
+		UE_LOG(LogCommonUI, Error, TEXT("CommonUI_FunctionLibrary::GetWidgetClassByTagNo: Empty Tag"));
+		return nullptr;
+	}
+
+	const UCommonUI_DeveloperSettings* Settings = GetDefault<UCommonUI_DeveloperSettings>();
+
+	TSoftClassPtr<UBaseActivatableWidget> ClassPtr = Settings->WidgetClassByTag.FindRef(Tag);
+	if (ClassPtr.IsNull())
+	{
+		UE_LOG(LogCommonUI, Error, TEXT("No widget for tag %s"), *Tag.ToString());
+		ensure(0);
+	}
+
+	return ClassPtr;
+}
---
+++ Source/LuShooter/Public/UI/CommonUI/CommonUI_FunctionLibrary.h
@@ -0,0 +1,19 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CommonUI_FunctionLibrary.generated.h"
+
+class UBaseActivatableWidget;
+
+UCLASS()
+class LUSHOOTER_API UCommonUI_FunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintPure, Category="UI")
+	static TSoftClassPtr<UBaseActivatableWidget> GetWidgetClassByTag(UPARAM(meta=(Categories="UI.Widget")) const FGameplayTag Tag);
+};
