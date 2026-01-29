242890feb1d74a2e3ca2c713f2ad24668c818bb9
Ilia Lukin <ilia64mods@gmail.com>
2026-01-29 23:23:03 +0300

    CommonUI: Default Text Button

---
+++ Source/LuShooter/Private/UI/Components/Button/ButtonText.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+
+#include "UI/Components/Button/ButtonText.h"
+
+#include "CommonTextBlock.h"
+
+void UButtonText::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	SetDisplayText(DisplayText);
+}
+
+void UButtonText::SetDisplayText(const FText InDisplayText)
+{
+	if (!InDisplayText.IsEmpty() && TextBlock)
+	{
+		DisplayText = InDisplayText;
+		TextBlock->SetText(UseUpperCaseForDisplayText ? InDisplayText.ToUpper() : InDisplayText);
+	}
+}
---
+++ Source/LuShooter/Public/UI/Components/Button/ButtonText.h
@@ -0,0 +1,35 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserve
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "ButtonText.generated.h"
+
+class UCommonTextBlock;
+
+UCLASS(Abstract, Blueprintable, meta=(DisableNativeTick))
+class LUSHOOTER_API UButtonText : public UCommonButtonBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetDisplayText(const FText InDisplayText);
+
+protected:
+	virtual void NativePreConstruct() override;
+
+private:
+	UPROPERTY(meta=(BindWidgetOptional))
+	TObjectPtr<UCommonTextBlock> TextBlock;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Button")
+	FText DisplayText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Button")
+	bool UseUpperCaseForDisplayText{true};
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Button")
+	FText DescriptionText;
+};
